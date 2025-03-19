1991d1d8206f8bf38e8011e99a4014fbfa5a1bfe
POBOROJO <parijatbhatt1@gmail.com>
2025-03-20 05:06:46 +0530

    Add express server setup with middleware

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello World');
+import express from "express";
+import http from "http";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import compression from "compression";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+
+const server = http.createServer(app);
+
+
+server.listen(8080,()=>{
+    console.log("Server started on port http://localhost:8080");
+})
